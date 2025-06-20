dd236b089ce48ae185cf08bf02fd74c5159ce3fe
Lavneet Sharma <elavneet@gmail.com>
2025-06-20 20:49:39 +0530

    wrong newline

--- ios/StripeTerminalReactNative.swift
+++ ios/StripeTerminalReactNative.swift
@@ -1130,19 +1130,13 @@ class StripeTerminalReactNative: RCTEventEmitter, DiscoveryDelegate, MobileReade
             // Now write the URL to the NFC tag
             self?.writeNDEFURLToTag(url: url, tagId: collectedData.nfcUid) { writeSuccess, writeError in
                 if let writeError = writeError {
-                    // var result = ["collectedData": Mappers.mapFromCollectedData(collectedData)]
-                    // result["error"] = Errors.createError(code: ErrorCode.unexpectedSdkError, message: "Failed to write URL to NFC tag: \(writeError.localizedDescription)")
-
-                    let errorDict = Errors.createError(code: ErrorCode.unexpectedSdkError, message: "Failed to write URL to NFC tag:
-     \(writeError.localizedDescription)")
+                    let errorDict = Errors.createError(code: ErrorCode.unexpectedSdkError, message: "Failed to write URL to NFC tag: \(writeError.localizedDescription)")
                     let result: [String: Any] = [
                       "collectedData": Mappers.mapFromCollectedData(collectedData),
                       "error": errorDict["error"] ?? [:]
                     ]
                     resolve(result)
                 } else {
-                    // var result = ["collectedData": Mappers.mapFromCollectedData(collectedData)]
-                    // result["writeSuccess"] = writeSuccess
                     let result: [String: Any] = [
                       "collectedData": Mappers.mapFromCollectedData(collectedData),
                       "writeSuccess": writeSuccess
